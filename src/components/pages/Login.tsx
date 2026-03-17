4cedc0c6df574051e82886bf458bf66904cec577
Bhabani Shankar <shankarbhabani@gmail.com>
2026-03-17 19:04:00 +0530

    update

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { loginSuccess } from "@/store/authSlice";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-
+import loginImage from "../../assets/upscaled_image.png";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -54,65 +54,81 @@ const Login = () => {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center bg-blue-100">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-        </CardHeader>
+   <div className="flex h-screen">
 
-        <CardContent className="space-y-4">
-          {/* Email */}
-          <div>
-            <label>Email :</label>
-            <Input
-              type="email"
-              placeholder="Enter Email"
-              name="email"
-              value={data.email}
+  {/* LEFT SIDE (FORM) */}
+  <div className="w-full lg:w-[35%] flex items-center justify-center bg-blue-100">
+    <Card className="w-[90%] max-w-[350px] shadow-lg">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+      </CardHeader>
+
+      <CardContent className="space-y-4">
+        
+        {/* Email */}
+        <div>
+          <label>Email :</label>
+          <Input
+            type="email"
+            placeholder="Enter Email"
+            name="email"
+            value={data.email}
+            onChange={handleOnchange}
+          />
+        </div>
+
+        {/* Password */}
+        <div>
+          <label>Password :</label>
+
+          <div className="bg-slate-100 p-2 flex items-center rounded-md">
+            <input
+              type={showpassword ? "text" : "password"}
+              placeholder="Enter Password"
+              name="password"
+              value={data.password}
               onChange={handleOnchange}
+              className="w-full outline-none bg-transparent"
             />
-          </div>
 
-          {/* Password */}
-          <div>
-            <label>Password :</label>
-
-            <div className="bg-slate-100 p-2 flex items-center rounded-md">
-              <input
-                type={showpassword ? "text" : "password"}
-                placeholder="Enter Password"
-                name="password"
-                value={data.password}
-                onChange={handleOnchange}
-                className="w-full outline-none bg-transparent"
-              />
-
-              <div
-                className="cursor-pointer text-lg"
-                onClick={() => setShowPassword((prev) => !prev)}
-              >
-                {showpassword ? <FaEyeSlash /> : <FaEye />}
-              </div>
-            </div>
-
-            <Link
-              to="/forgot-password"
-              className="block w-fit ml-auto hover:underline hover:text-red-600"
+            <div
+              className="cursor-pointer text-lg"
+              onClick={() => setShowPassword((prev) => !prev)}
             >
-              Forgot Password ?
-            </Link>
+              {showpassword ? <FaEyeSlash /> : <FaEye />}
+            </div>
           </div>
 
-          <Button
-            className="w-full bg-blue-600"
-            onClick={handleLogin}
-            disabled={!data.email || !data.password}
+          <Link
+            to="/forgot-password"
+            className="block w-fit ml-auto hover:underline hover:text-red-600"
           >
-            Login
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
+            Forgot Password ?
+          </Link>
+        </div>
+
+        <Button
+          className="w-full bg-blue-600"
+          onClick={handleLogin}
+          disabled={!data.email || !data.password}
+        >
+          Login
+        </Button>
+
+      </CardContent>
+    </Card>
+  </div>
+
+  {/* RIGHT SIDE IMAGE */}
+  <div className="hidden lg:block w-[65%] h-full">
+    <img
+      src={loginImage}
+      alt="login"
+      className="w-full h-full object-cover"
+    />
+  </div>
+
+</div>
   );
 };
 
